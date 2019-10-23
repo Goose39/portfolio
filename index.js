10428ef967e5c8ca693f1ddcc38263c57b623157
Goose39 <Goose39.Dev@gmail.com>
2019-10-23 17:24:54 -0400

    contact form js and minor css added

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
+function handlejs() {
+    handleThumbnailClicks();
+    mailClick();
+    textClick();
+    formSubmit();
+}
+
+function textClick() {
+    $(".contact").on("click", ".msg-text", function() {
+        str = $(".msg-text").val();
+        if (str === "Send me an email, I would be happy to hear from you!") {
+            $(".msg-text").val("");
+        }
+    })
+}
+
 function handleThumbnailClicks() {
     $(".screenshot").on("click", ".thumbnail", function(event) {
         const src = $(this).attr("src");
@@ -6,4 +22,29 @@ function handleThumbnailClicks() {
     })
 }; 
 
-$(handleThumbnailClicks);
+function mailClick() {
+    $(".social").on("click", ".mail-icon", function(event) {
+        window.location.href = `${genE("Hi Shaun, ")}`;
+    })
+}
+
+function genE(msg) {
+    let str = "";
+        str = str + "mail" + "to:" + "goose39.dev" + "@" + "gmail" + ".com?";
+        str += `subject=Portfolio-Site-Message&body=${msg}`
+        return str
+}
+
+function formSubmit() {
+    $(".submit").on("click", function(event) {
+        event.preventDefault();
+        let msg = $(".msg-text").val();
+        window.location.href = `${genE(msg)}`
+    })
+   
+}
+
+
+
+
+$(handlejs);
